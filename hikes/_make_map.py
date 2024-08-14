85cb29e3421d7de43e6302c05cd3cdc07f411095
Jochem Smit <jhsmit@gmail.com>
2024-08-14 18:22:20 +0200

    add hikes and first hike post

--- hikes/_make_map.py
+++ hikes/_make_map.py
@@ -1,12 +1,14 @@
+# %%
 from pathlib import Path
 
 import gpxpy
 from ipyleaflet import Map, Marker, Polyline
-from ipywidgets import Layout
+from ipywidgets import HTML, Layout
 
 AMS = (2.377956, 4.897070)
 
 
+# %%
 def parse_gpx(file_path: Path) -> list[tuple[float, float]]:
     gpx = gpxpy.parse(file_path.read_text())
 
@@ -26,12 +28,32 @@ def load_coords(dir_path: Path) -> list[tuple[float, float]]:
     return coords
 
 
-dpath = Path("deadly-shenanigans")
-coords = load_coords(dpath)
+# %%
 m = Map(center=AMS, zoom=2, scroll_wheel_zoom=True, layout=Layout(height="600px"))
-gpx_track = Polyline(locations=coords, color="blue", fill=False, opacity=0.7)
-gpx_marker = Marker(location=coords[0], draggable=False)
 
-m.add_layer(gpx_track)
-m.add_layer(gpx_marker)
+
+def valid(p: Path) -> bool:
+    if p.stem.startswith("."):
+        return False
+    if p.is_dir():
+        return True
+    return False
+
+
+dirs = [d for d in Path("hikes-gpx").iterdir() if valid(d)]
+for dpath in dirs:
+    coords = load_coords(dpath)
+    gpx_track = Polyline(locations=coords, color="blue", fill=False, opacity=0.7)
+    gpx_marker = Marker(location=coords[0], draggable=False)
+    if (Path("posts") / (dpath.stem + ".qmd")).exists():
+        hike_name = dpath.stem.replace("-", " ").title()
+        popup_html = f"""
+        <h3>{hike_name}</h3>
+        <a href="/hikes/posts/{dpath.stem}.html">View Details</a>
+        """
+        gpx_marker.popup = HTML(popup_html)
+    m.add_layer(gpx_track)
+    m.add_layer(gpx_marker)
 m
+
+# %%
