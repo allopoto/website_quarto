1454704dea7e474ebd241145b840b1fde834ddd1
Jochem Smit <jhsmit@gmail.com>
2025-02-14 14:34:07 +0100

    add fkl hike, alternate colors

--- hikes/_make_map.py
+++ hikes/_make_map.py
@@ -1,5 +1,6 @@
 # %%
 from pathlib import Path
+from typing import Iterable
 
 import gpxpy
 from ipyleaflet import Map, Marker, MarkerCluster, Polyline
@@ -9,6 +10,16 @@ AMS = (2.377956, 4.897070)
 
 
 # %%
+
+
+def valid(p: Path) -> bool:
+    if p.stem.startswith("."):
+        return False
+    if p.is_dir():
+        return True
+    return False
+
+
 def parse_gpx(file_path: Path) -> list[tuple[float, float]]:
     gpx = gpxpy.parse(file_path.read_text())
 
@@ -28,24 +39,33 @@ def load_coords(dir_path: Path) -> list[tuple[float, float]]:
     return coords
 
 
+def make_tracks(segment_coords: Iterable[list[tuple[float, float]]]) -> list[Polyline]:
+    tracks = []
+    for i, segment in enumerate(segment_coords):
+        gpx_track = Polyline(
+            locations=segment,
+            color="blue" if i % 2 == 0 else "green",
+            fill=False,
+            opacity=0.7,
+            scaling=False,
+            rotations=False,
+        )
+        tracks.append(gpx_track)
+    return tracks
+
+
 # %%
 m = Map(center=AMS, zoom=2, scroll_wheel_zoom=True, layout=Layout(height="600px"))
-
-
-def valid(p: Path) -> bool:
-    if p.stem.startswith("."):
-        return False
-    if p.is_dir():
-        return True
-    return False
-
 
 dirs = [d for d in Path("hikes-gpx").iterdir() if valid(d)]
 markers = []
 for dpath in dirs:
-    coords = load_coords(dpath)
-    gpx_track = Polyline(locations=coords, color="blue", fill=False, opacity=0.7)
-    gpx_marker = Marker(location=coords[0], draggable=False)
+    segment_coords = [parse_gpx(fpath) for fpath in sorted(dpath.glob("*.gpx"))]
+    tracks = make_tracks(segment_coords)
+    for track in tracks:
+        m.add(track)
+    # gpx_track = Polyline(locations=coords, color="blue", fill=False, opacity=0.7)
+    gpx_marker = Marker(location=segment_coords[0][0], draggable=False)
     if (Path("posts") / (dpath.stem + ".qmd")).exists():
         hike_name = dpath.stem.replace("-", " ").title()
         popup_html = f"""
@@ -54,9 +74,8 @@ for dpath in dirs:
         """
         gpx_marker.popup = HTML(popup_html)
 
-    m.add(gpx_track)
+    # m.add(gpx_track)
     markers.append(gpx_marker)
-
 
 m.add(MarkerCluster(markers=markers))
 
